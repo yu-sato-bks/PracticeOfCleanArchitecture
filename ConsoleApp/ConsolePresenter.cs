b0fb53c31066df5b322e705de92ff5dca12aabba
yu_sato <yu_sato@bs-ja.co.jp>
2023-08-31 14:55:54 +0900

    ConsoleApp追加

---
+++ ConsoleApp/ConsolePresenter.cs
@@ -0,0 +1,17 @@
+﻿using UseCase;
+
+namespace ConsoleApp;
+
+public class ConsolePresenter : IUserCreatePresenter
+{
+    public void Progress(int percentage)
+    {
+        Console.WriteLine(percentage + "%");
+    }
+
+    public void Complete(UserCreateResponse response)
+    {
+        Console.WriteLine("complete");
+        Console.WriteLine("created id: " + response.UserId);
+    }
+}
